7cccd4af9c911d7d664dc621dcfdac129efcffe4
Csaba Babos <babosc@gmail.com>
2014-10-10 19:48:48 +0200

    FIX an issue with i3bar mpc current play

--- .i3status.sh
+++ .i3status.sh
@@ -3,5 +3,5 @@ i3status --config ~/.i3status.conf | while :
 do
         read line
         playing=$(mpc current)
-        echo "$playing | $line" || exit 1
+        echo "$playing $line" || exit 1
 done
