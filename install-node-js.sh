2fc443abeb25713a2ce9bb73a05c9a970311e2b1
Csaba Babos <babosc@gmail.com>
2014-11-21 13:32:56 +0100

    Fixed a really big bug with prefix and added npm

--- install-node-js.sh
+++ install-node-js.sh
@@ -8,9 +8,12 @@ git clone git://github.com/joyent/node.git ~/node-installer
 
 # Building and installing node.js
 cd ~/node-installer
-./configure --prefix=~/local
+./configure --prefix=$HOME
 make
 make install
+
+# Install npm
+curl -L https://npmjs.org/install.sh | sh
 
 # Install my necessary tools in node
 npm -g install browser-sync gulp yo generator-webapp
