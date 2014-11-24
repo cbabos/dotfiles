8dd13e34c97547682d80eb89d52a165cdaedc078
Csaba Babos <babosc@gmail.com>
2014-11-24 11:28:45 +0100

    Changed node.js install method

--- install-node-js.sh
+++ install-node-js.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-# The target directory to install to
-mkdir ~/local
-
-# Cloning the official git repo
-git clone git://github.com/joyent/node.git ~/node-installer
-
-# Building and installing node.js
-cd ~/node-installer
-./configure --prefix=$HOME
-make
-make install
-
-# Install npm
-curl -L https://npmjs.org/install.sh | sh
+cd ~
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+source ~/.nvm/nvm.sh
+nvm install 0.10
+nvm alias default 0.10
 
 # Install my necessary tools in node
 npm -g install browser-sync gulp yo generator-webapp
