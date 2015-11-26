9c2ed3b5f3a42d09b7f21274ab7f6690128bced4
Babos Csaba <babos.csaba@yourweb.hu>
2015-11-26 21:47:29 +0100

    Updated dotfile

--- setup.sh
+++ setup.sh
@@ -15,14 +15,16 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 copyDotFiles()
 
 # Updating and upgrading brew
-# Later (after we activated zsh with oh-my-zsh) you can use `bubu` alias for this 
-brew update && brew upgrade
+# Later (after we activated zsh with oh-my-zsh) you can use `bubu` alias for this
+
+brew update
+brew upgrade
 
 # Installing basic toolchain
 brew install coreutils moreutils findutils zsh homebrew/dupes/grep caskroom/cask/brew-cask ack pv git lynx nmap pigz rename rhino tree webkit2png zopfli p7zip vim --override-system-vi imagemagick --with-webp lua node
 
 # Installing Native Apps
-brew cask install android-studio google-chrome imagealpha imageoptim libreoffice mysqlworkbench skype squire steam transmission trim-enabler vlc
+brew cask install android-studio google-chrome iterm2 imagealpha imageoptim libreoffice mysqlworkbench skype squire steam spotify
 
 # Installing Node.JS components that I need
 npm install -g gulp browser-sync bower grunt grunt-cli yo generator-webapp
@@ -41,3 +43,11 @@ vim -c "BundleInstall" -c "qa"
 
 # Don't create .DS_Store files on network shares (need logout / login) 
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
+# Install Inconsolata font
+cp "Inconsolata-g for Powerline.otf" ~/Library/Fonts/
+
+# Install iTerm2 color scheme
+open tomorrow-night-eighties.itermcolors 
+
+
