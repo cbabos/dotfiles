14c9d7721f240443933ffbcbd0a1cb6bf0e56629
Babos Csaba <babos.csaba@yourweb.hu>
2021-09-19 14:13:04 +0100

    Cleanup, create general (non-OSX) version

--- README.md
+++ README.md
@@ -3,12 +3,7 @@ This is a simple dotfiles repo for my desktop.
 It's containing personal configuration so feel free to modify it if you fork it. 
 
 ## Basic environment
-* [Homebrew](http://brew.sh/)
-* [Homebrew Cask](http://caskroom.io/)
 * [Node.JS](http://nodejs.org/)
-* [Gulp](http://gulpjs.com)
-* [Browsersync](http://browsersync.io/)
-* [Yeoman](http://yeoman.io/)
 * [VIM](http://www.vim.org/)
 * [Oh My Zsh](http://ohmyz.sh/)
 
--- setup.sh
+++ setup.sh
@@ -8,22 +8,8 @@ function copyDotFiles() {
 # Install oh-my-zsh
 curl -L http://install.ohmyz.sh | sh
 
-# Install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 # Install my dotfiles
 copyDotFiles()
-
-# Updating and upgrading brew
-# Later (after we activated zsh with oh-my-zsh) you can use `bubu` alias for this
-brew update 
-brew upgrade
-
-# Installing basic toolchain
-brew install coreutils moreutils findutils zsh homebrew/dupes/grep caskroom/cask/brew-cask ack pv git lynx nmap pigz rename rhino tree webkit2png zopfli p7zip vim --override-system-vi imagemagick --with-webp lua node tmux
-
-# Installing Native Apps
-brew cask install java android-studio google-chrome iterm2 imagealpha imageoptim libreoffice skype steam 
 
 # Installing Node.JS components that I need
 npm install -g gulp browser-sync bower 
@@ -37,19 +23,6 @@ if [ ! -d "${vundle_dir}" ]; then
 
 # Install the plugins
 vim -c "BundleInstall" -c "qa"
-
-# Some OSX settings
-
-# Don't create .DS_Store files on network shares (need logout / login) 
-defaults write com.apple.desktopservices DSDontWriteNetworkStores true
-
-# Install iTerm2 color scheme
-open dimmedmonokai.itermcolors 
-
-# Install Powerline fonts
-git clone https://github.com/powerline/fonts.git ~/fonts
-~/fonts/install.sh
-rm -Rf ~/fonts
 
 # Create tmux config folder
 mkdir -p ~/.tmux/plugins
