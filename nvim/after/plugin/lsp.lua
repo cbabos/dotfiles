a6f075c73e2327885f20647940b45405e0157004
Babos Csaba <babos.csaba@yourweb.hu>
2024-11-29 21:00:00 +0100

    Updated dotfiles - autobackup

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -8,7 +8,7 @@ end)
 
 -- to learn how to use mason.nvim
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-require('mason').setup({})
+require('mason').setup({ })
 require('mason-lspconfig').setup({
   ensure_installed = {},
   handlers = {
